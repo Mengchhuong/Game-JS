3317af1cfb2e5c755e7543cd52f312fdf3dbb7a2
Mengchhuong Ang <Ang Mengchhuong>
2020-12-18 17:54:11 +0700

    version3

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,11 @@ var context = canvas.getContext('2d');
 let moveUp=false,moveDown=false;
 var x = canvas.width/2;
 var y = canvas.height/2;
-var dx = 2;
+var dx = 5;
 var dy = -2;
+var score =0;
+var userscore=0;
+let ball;
 
 function background() {
     // set a style
@@ -19,11 +22,18 @@ function background() {
   background();
 
 //draw ball
-function drawBall(){
-    context.beginPath();
-    context.arc(x, y, 10, 0, Math.PI *2);
-    context.fillStyle = "#FFFFFF";
-    context.fill();
+function drawBall(x,y){
+    return{
+        x:x,
+        y:y,
+        update:function(){
+            context.beginPath();
+            context.arc(this.x, this.y, 10, 0, Math.PI *2);
+            context.fillStyle = "#FFFFFF";
+            context.fill();
+        }
+    }
+   
 }
 
 //draw paddle
@@ -82,21 +92,26 @@ function randomEnemy(paddle){
                 paddle.y=0;
             }
         }
-    },50);
+    },100);
 }
-
-
 randomEnemy(paddleEnemy);
 
-function draw(paddle) {
+function draw() {
     // context.clearRect(0, 0, canvas.width, canvas.height);
     erase();
-    drawBall();
+    // drawBall(x,y);
+    ball = drawBall(x,y)
+    ball.update();
     paddleEnemy.draw();
     paddleUser.draw();
     
     if(x + dx > canvas.width-10 || x + dx < 10) {
+        // x= x-12;
+        // x= canvas.width +20;
         dx = -dx;
+       
+        // comScore.play();
+        
     }
     if(y + dy > canvas.height-10 || y + dy < 10) {
         dy = -dy;
@@ -104,21 +119,50 @@ function draw(paddle) {
     x += dx;
     y += dy;
 
-    var randomUp = canvas.height - paddle.h; 
-        if(paddle.y>=randomUp){
-            paddle.y = randomUp;
-            paddle.y-=10;
-        }
-        else if(paddle.y>=0 && paddle.y<320){
-            paddle.y+=10;
-            if(paddle.y==320){
-                paddle.y=0;
-            }
-        }
+    if(paddleUser.x<x+10&&paddleUser.y<y+10&&paddleUser.x+paddleUser.w&&x-10&&paddleUser.y+paddleUser.h>x-10&&paddleUser.y+paddleUser.h>y-10){
+        console.log(x)
+        y -=dy;
+        // dy = -dy;
+    }
+    else if ( x  == 10){
+        score++;
+        x = canvas.width/2;
+        y = canvas.height/2;
+        ball = drawBall(x,y)
+        ball.update();
+        dx =  5;
+        dy = -2;
+    }
+    else if ( x ==690 ){
+        userscore++;
+        x = canvas.width/2;
+        y = canvas.height/2;
+        ball = drawBall(x,y)
+        ball.update();
+        dx = -5;
+        dy = 2;
+    }
+   
+    // console.log(score);
+    // console.log(userscore);
+
+// function isCollision(){
+//     return(paddleUser.x<x+10&&paddleUser.y<y+10&&paddleUser.x+paddleUser.w&&x-10&&paddleUser.y+paddleUser.h>x-10&&paddleUser.y+paddleUser.h>y-10)
+// }
+//     // var randomUp = canvas.height; 
+//         // if(paddle.y>=canvas.height){
+//         //     // paddle.y = randomUp;
+//         //     paddle.y-=10;
+//         // }
+//         // else if(paddle.y>=0 && paddle.y<320){
+//         //     paddle.y+=10;
+//         //     if(paddle.y==320){
+//         //         paddle.y=0;
+//         //     }
+//         // }
   
 }
-
-setInterval(draw, 20);
+setInterval(draw, 50);
 
 function startGame(){
 
@@ -132,4 +176,41 @@ function startGame(){
     // draw();
     
 }
+// function isCollision(){
+//     // return (x+10>paddleUser.x&&paddleUser.x+paddleUser.w>x);
+//     return{
+//         updateRightUp:function(){
+//             drawBall();
+//             x+=dx;
+//             y+=dy;
+//         },
+//         updateRightDown:function(){
+//             drawBall();
+//             x+=dx;
+//             y-=dy;
+//         },
+//         updateLeftUp:function(){
+//             drawBall();
+//             x-=dx;
+//             y+=dy;
+//         },
+//         updateLeftDown:function(){
+//             drawBall();
+//             x-=dx;
+//             y-=dy;
+//         }
 
+//     }
+    
+// }
+
+// function update(){
+//     console.log(x);
+    
+//     if( x - 10 < 0){
+//         user.score++;
+//         userScore.play();
+//         resetBall();
+//     }
+// }
+// update();
